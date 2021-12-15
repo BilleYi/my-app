e8fbc65d804330a992b720dfb982146bdd90b3fa
BilleYi <bille.yi@silksoftware.com>
2021-12-15 18:11:02 +0800

    fix: fix bug

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import { Layout } from 'antd'
-import { Provider } from 'react-redux'
-import { BrowserRouter } from 'react-router-dom'
-import Router from './router'
-import AppHeader from './components/Header'
-import AppFooter from './components/Footer'
-import Login from './components/Login'
-import store from './store'
+import React from "react"
+import ReactDOM from "react-dom"
+import "./index.css"
+import { Layout } from "antd"
+import { Provider } from "react-redux"
+import { BrowserRouter } from "react-router-dom"
+import Router from "./router"
+import AppHeader from "./components/Header"
+import AppFooter from "./components/Footer"
+import Login from "./components/Login"
+import store from "./store"
 
 const { Header, Footer, Content } = Layout
 
-const App = function() {
+const App = function () {
   return (
     <BrowserRouter>
-      <Layout style={{ minWidth: '1000px' }}>
+      <Layout style={{ minWidth: "1000px" }}>
         <Header className="header">
           <AppHeader />
           <Login />
@@ -37,5 +37,5 @@ ReactDOM.render(
   // <React.StrictMode>
   <App />,
   // </React.StrictMode>,
-  document.getElementById('root'),
+  document.getElementById("root")
 )
